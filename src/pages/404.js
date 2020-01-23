d5822d622dacbd446b3e55fe1d4ddc97900d1fa8
Karoline Alencar <karolinedalencar@gmail.com>
2020-01-23 12:16:31 -0300

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,44 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+import * as S from "../components/NotFoundWrapper/styled"
+
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "pluto-sign-in.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 480) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <S.NotFoundWrapper>
+        <S.NotFoundWrapperTitle>Nothing Here... :(</S.NotFoundWrapperTitle>
+        <S.NotFoundImgWrapper
+          href="https://icons8.com"
+          title="Illustration by Ouch.pics"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+        </S.NotFoundImgWrapper>
+        <S.NotFoundWrapperLink to="/" title="Go to home">
+          Go home...
+        </S.NotFoundWrapperLink>
+      </S.NotFoundWrapper>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
